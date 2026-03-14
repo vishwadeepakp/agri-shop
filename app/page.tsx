e22bbad4a594f004543016be274f93ec48b06945
saloni <saloni18heatabit@gmail.com>
2026-03-14 18:56:55 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <main className="flex-1">
         <HeroSection />
         <Certifications />
+        
         {/* <AboutSection /> */}
         <ProductCategories />
         <LocationContact />
